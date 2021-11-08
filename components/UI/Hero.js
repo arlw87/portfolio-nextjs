0ccb34e757fdd746c1c3fd9b6428c9949c929b15
arlw87 <arlw87@googlemail.com>
2021-11-08 09:42:35 +0000

    feat: Hero Banner for Mobile
    Basic Hero Banner for Mobile Layout

--- components/UI/Hero.js
+++ components/UI/Hero.js
@@ -2,9 +2,11 @@ import styles from "./Hero.module.css";
 
 const Hero = () => {
   return (
-    <div>
-      <h1>Andrew White</h1>
-      <h1>Front End & Software Developer</h1>
+    <div className={styles.heroContainer}>
+      <h1 className={styles.name}>Andrew White</h1>
+      <div className={styles.job}>
+        <h1>Front End </h1> <h1>&</h1> <h1>Software Developer</h1>
+      </div>
       <button>Find Out More</button>
     </div>
   );
